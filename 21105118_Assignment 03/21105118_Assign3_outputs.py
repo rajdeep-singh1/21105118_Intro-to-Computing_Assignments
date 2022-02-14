850364bb569aeaadee6a1f6f248a271e0223bf63
Rajdeep Singh <97239975+rajdeep-singh1@users.noreply.github.com>
2022-02-15 00:03:01 +0530

    Add files via upload

---
+++ 21105118_Assignment 03/21105118_Assign3_outputs.py
@@ -0,0 +1,95 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+============================================= RESTART: C:/Users/arcco/OneDrive/Documents/21105118_Assignment 03/21105118_Assign3_outputs.py ============================================
+Que1 - Output
+
+Enter the String : python is object oriented programming language,python also supports various machine learning libraries
+Dictionary containing {'Letter':'Letter Count'} is : {'supports': 1, 'also': 1, 'is': 1, 'language,python': 1, 'programming': 1, 'machine': 1, 'learning': 1, 'libraries': 1, 'oriented': 1, 'object': 1, 'various': 1, 'python': 1}
+
+============================================= RESTART: C:/Users/arcco/OneDrive/Documents/21105118_Assignment 03/21105118_Assign3_outputs.py ============================================
+Que1 - Output(Single Word Case)
+
+Enter the String : ObjectOriented
+Dictionary containing {'Letter':'Letter Count'} is : {'o': 2, 'r': 1, 't': 2, 'n': 1, 'e': 3, 'b': 1, 'j': 1, 'c': 1, 'd': 1, 'i': 1}
+
+============================================= RESTART: C:/Users/arcco/OneDrive/Documents/21105118_Assignment 03/21105118_Assign3_outputs.py ============================================
+Que2 - Output
+
+Enter the day [1-31] : 31
+Enter the month [1-12] : 12
+Enter the year [1800-2025] : 2022
+Next Date is: 1/01/2023.
+
+============================================= RESTART: C:/Users/arcco/OneDrive/Documents/21105118_Assignment 03/21105118_Assign3_outputs.py ============================================
+Que3 - Output
+
+Instruction! Press X or x to exit
+Enter any Digit : 15
+Enter any Digit : 48
+Enter any Digit : 47
+Enter any Digit : 56
+Enter any Digit : x
+[(15, 225), (48, 2304), (47, 2209), (56, 3136)]
+
+============================================= RESTART: C:/Users/arcco/OneDrive/Documents/21105118_Assignment 03/21105118_Assign3_outputs.py ============================================
+Que4 - Output
+
+Hey User, Enter the grade : 10
+Your grade is 'A+' and Outstanding Performance.
+
+============================================= RESTART: C:/Users/arcco/OneDrive/Documents/21105118_Assignment 03/21105118_Assign3_outputs.py ============================================
+Que5 - Output
+
+ABCDEFGHIJK
+ ABCDEFGHI 
+  ABCDEFG  
+   ABCDE   
+    ABC    
+     A     
+
+============================================= RESTART: C:/Users/arcco/OneDrive/Documents/21105118_Assignment 03/21105118_Assign3_outputs.py ============================================
+Que6 - Output
+
+Please enter the name of the Student - Rajdeep
+Please Enter SID of Student - 21105118
+Either press Y to continue or N to terminate : y
+Please enter the name of the Student - Rahul
+Please Enter SID of Student - 21105117
+Either press Y to continue or N to terminate : n
+
+Ques.6(a) The Dictionary containing {'SID':'Name'} is : {21105118: 'Rajdeep', 21105117: 'Rahul'} 
+
+Ques.6(b) The Dictionary after sorting according to name : {21105117: 'Rahul', 21105118: 'Rajdeep'}
+
+Ques.6(c) The Dictionary after sorting according to SID : {21105117: 'Rahul', 21105118: 'Rajdeep'}
+
+Ques.6(d)
+Enter SID to get name of student : 21105118
+Name of student with SID 21105118 is Rajdeep.
+
+============================================= RESTART: C:/Users/arcco/OneDrive/Documents/21105118_Assignment 03/21105118_Assign3_outputs.py ============================================
+Que7 - Output
+
+Total elements of Fibonacci sequence that you want(must be greater than 1)- 14
+Fibonnaci sequence with 14 terms
+0 1 1 2 3 5 8 13 21 34 55 89 144 233 
+Average of total 14 terms of sequence is 43.5
+end
+
+============================================= RESTART: C:/Users/arcco/OneDrive/Documents/21105118_Assignment 03/21105118_Assign3_outputs.py ============================================
+Que8 - Output
+
+Set1= {1, 2, 3, 4, 5}
+Set2= {8, 2, 4, 6}
+Set3= {1, 17, 5, 9, 13}
+
+Ques.8(a) Set of all 'elements that are in Set1 and Set2 but not both' is : {1, 3, 5, 6, 8}
+
+Ques.8(b) A new set of all elements that are 'in only one of the three sets Set1,Set2 and Set3' is : {3, 6, 8, 9, 13, 17}
+
+Ques.8(c) A new set of elements that are 'exactly in two of the sets Set1, Set2 and Set3' is : {1, 2, 4, 5}
+
+Ques.8(d) A new set of all Integers in the 'range 1 to 10' that are 'not in Set1' is : {6, 7, 8, 9, 10} 
+
+Ques.8(e) A new set of all Integers in the range 1 to 10 that are not in Set1,Set2 and Set3 : {10, 7}
